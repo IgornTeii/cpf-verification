296137ac31a2fd794a29a361d9eeda4be372723d
Igor <logonrmlocal@fiap.com.br>
2023-08-07 10:54:50 -0300

    cpf verified

--- cpf.py
+++ cpf.py
@@ -1,22 +1,29 @@
-def valida1(n1):
+def valida1(n1, x):
     soma = digito = resto = 0
-    cont = 10
+    cont = x
     for i in n1:
         soma += i * cont
         cont -= 1
-resto = soma % 11
-if resto < 2:
-    digito = 0
-else 
-    digito = 11 - resto
-return digito
-
+    resto = soma % 11
+    if resto < 2:
+        digito = 0
+    else :
+        digito = 11 - resto
+    return digito
 
 #programa principal
+
 cpfnum = []
+digiuno = digidou = 0 
 print("Digite seu CPF (somente numeros)")
 cpf = input()
 for i in range(0,9,1):
     cpfnum.append(int(cpf[i:i+1]))
-#print(cpfnum)
+digiuno =valida1(cpfnum, 10)
+cpfnum.append(digiuno)
+digidou = valida1(cpfnum, 11)
+cpfnum.append(digidou)
+print(cpfnum)
+
+
 
